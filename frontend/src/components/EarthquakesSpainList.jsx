e6f80f15bcfcdc66ebe76ed3df2f0af311249a77
jxsem <josemanuel.soldado@outlook.es>
2026-09-25 12:46:43 +0200

    docs: comments functions

--- frontend/src/components/EarthquakesSpainList.jsx
+++ frontend/src/components/EarthquakesSpainList.jsx
@@ -4,13 +4,18 @@ import { useState, useEffect } from "react";
 import EarthquakeCardSpain from "./EarthquakeCardSpain";
 
 function EarthquakeSpainList() {
-    // el useState guarda los terremotos que nos devuelve fastAPI
+    // el useState guarda los terremotos que nos devuelve fastAPI.. Guarda datos que pueden cambiar y que React necesita recordar.
     const [earthquakeSpain, setEarthquakeSpain] = useState([]);
 
-    // cuando aparece el componente, es el momento de hacer la peticion API
-    useEffect(()=>{
+    // cuando aparece el componente, es el momento de hacer la peticion API.. Es para ejecutar código después de que React haya renderizado el componente, normalmente como respuesta a cambios en ciertas dependencias.
+    useEffect(() => {
         fetch("http://localhost:8000/earthquakes/spain")
+            // fetch devuelve una Promise. Cuando la petición termina,
+            // el resultado se recibe en "response".
             .then((response) => response.json())
+            // response.json() también devuelve una Promise.
+            // Cuando termina, el JSON convertido a JavaScript
+            // se recibe en "data".
             .then((data) => {
                 setEarthquakeSpain(data);
             });
@@ -21,6 +26,7 @@ function EarthquakeSpainList() {
         <div>
             <h1 className="m-2" >Terremotos de España</h1>
             {earthquakeSpain.map((terremoto) => (
+                //Esto son props, datos que un componente PADRE le pasa a un componente hijo... -> el padre es earthquakespainlist que le pasa al hijo earthquakecardspain
                 <EarthquakeCardSpain
                     key={terremoto.id}
                     localidad={terremoto.localidad}
